cc9f6f7cd0bfc4bd9f744c593a100ce6382426e4
dwljia <dwljia@qq.com>
2020-06-09 21:47:31 +0800

    feat 自适应

--- src/js/about.js
+++ src/js/about.js
@@ -1,15 +1,13 @@
 var defaultObj = {
   swiper: function () {
     var swiper = new Swiper('.swiper-container', {
-      direction: 'vertical',
       pagination: {
         el: '.swiper-pagination',
-        clickable: true,
       },
-      autoHeight: true,
-    })
+    });
   }
 }
 $(document).ready(function () {
   defaultObj.swiper()
 })
+
